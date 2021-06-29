cef87bd8fe1ccb8901de9c41a4f4630ba0d632e9
Donald Pinckney <donald_pinckney@icloud.com>
2021-06-29 12:50:32 -0400

    Deploy to GitHub Pages

--- books/build_all.sh
+++ books/build_all.sh
@@ -2,11 +2,6 @@
 
 cd books
 
-# First, if root sibling directory of mdbook exists, then rebuild mdbook if needed
-if [ -d "../../mdBook/" ]; then
-	pushd ../../mdBook/; ./install.sh; popd
-fi
-
 ./build_book.sh tensorflow
 ./build_book.sh pytorch
 # ./build_book.sh pl
